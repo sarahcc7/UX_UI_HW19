9e955eee3a279c8204bea3959e88ab4dc72174fe
sarahcc7 <usw879@my.utsa.edu>
2023-08-03 16:50:27 -0500

    Add bounce effect to PS image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log("JS Working");
+$( document ).ready(function() {
+    console.log("JS Working");
+    const image = $('#my-work-image')
+    image.on('mouseenter', function() {
+        // on mouseenter
+        image.addClass('animate__bounce')
+    })
+    image.on('mouseleave', function() {
+        // on mouseleave
+        image.removeClass('animate__bounce')
+    })
+});
